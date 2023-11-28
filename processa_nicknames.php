8989e7a81ea93acd105a85a73d1cab8fff2739d0
Victor Barbosa <victoraug09@gmail.com>
2023-11-28 08:11:50 -0300

    Update processa_nicknames.php

--- processa_nicknames.php
+++ processa_nicknames.php
@@ -1,59 +1,65 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-// Mostra erros, se houver algum.
+include 'functions.php';
 
-$filename = 'nicknames.txt';
+// Verifica se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Conectar ao banco de dados
+    $servername = "srv1079.hstgr.io";
+    $username = "u368907112_admin";
+    $password = "Victor270377@";
+    $dbname = "u368907112_smurfarena";
 
-// Lógica para adicionar nickname utilizando método POST.
-if (isset($_POST['add_button']) && isset($_POST['add_nickname'])) {
-    $nickname = $_POST['add_nickname'];
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Carrega o conteúdo atual.
-    $nicknames = file_get_contents($filename);
-
-    // Adiciona o novo nickname.
-    $nicknames .= $nickname . PHP_EOL;
-
-    // Salva o conteúdo atualizado no arquivo.
-    file_put_contents($filename, $nicknames);
-
-    echo "Nickname adicionado com sucesso.";
-    
-    header('Location: Contas.html');
-    exit(); // Recarrega a pagina após o fim da operação.
-}
-
-// Lógica para excluir nickname, utilizando método POST.
-if (isset($_POST['delete_button']) && isset($_POST['delete_nickname'])) {
-    $nicknameToDelete = $_POST['delete_nickname'];
-
-    // Carrega o conteúdo atual.
-    $nicknames = file_get_contents($filename);
-
-    // Remove o nickname especificado.
-    $nicknames = str_replace($nicknameToDelete . PHP_EOL, '', $nicknames);
-
-    // Salva o conteúdo atualizado.
-    file_put_contents($filename, $nicknames);
-
-    echo "Nickname excluído com sucesso.";
-    
-    header('Location: Contas.html');
-    exit(); // Recarrega a pagina após o fim da operação.
-    
-}
-
-// Exibir nicknames
-$nicknames = file_get_contents($filename);
-
-if (!empty($nicknames)) {
-    $nicknamesArray = explode(PHP_EOL, trim($nicknames));
-
-    foreach ($nicknamesArray as $index => $nickname) {
-        echo "<tr><td>" . ($index + 1) . "</td><td>" . $nickname . "</td></tr>";
+    // Verifica a conexão
+    if ($conn->connect_error) {
+        die("Conexão falhou: " . $conn->connect_error);
     }
-} else {
-    echo "<tr><td colspan='2'>Nenhum nickname registrado.</td></tr>";
+
+    // Adicionar ou excluir Nickname req post
+    if ((isset($_POST["add_button"]) && isset($_POST["add_nickname"])) || (isset($_POST["delete_button"]) && isset($_POST["delete_nickname"]))) {
+        $nickname = isset($_POST["add_button"]) ? $_POST["add_nickname"] : $_POST["delete_nickname"];
+        
+        // Adicionar ou excluir do banco de dados req post
+        if (isset($_POST["add_button"])) {
+            $sql = "INSERT INTO nicknames (nickname) VALUES ('$nickname')";
+            $success_message_db = "Nickname adicionado ao banco de dados com sucesso!";
+            $error_message_db = "Erro ao adicionar nickname ao banco de dados: " . $conn->error;
+        } elseif (isset($_POST["delete_button"])) {
+            $sql = "DELETE FROM nicknames WHERE nickname = '$nickname'";
+            $success_message_db = "Nickname excluído do banco de dados com sucesso!";
+            $error_message_db = "Erro ao excluir nickname do banco de dados: " . $conn->error;
+        }
+
+        if ($conn->query($sql) === TRUE) {
+            echo $success_message_db . "<br>";
+        } else {
+            echo $error_message_db . "<br>";
+        }
+
+        // Adicionar ou excluir do arquivo txt
+        $txtFilename = "nicknames.txt";
+        $search = "Novo Nickname: $nickname";
+        $txtContent = file_get_contents($txtFilename);
+
+        if (isset($_POST["add_button"])) {
+            $txtContent .= "Novo Nickname: $nickname\n";
+            $success_message_txt = "Nickname adicionado com sucesso!";
+            $error_message_txt = "Erro ao adicionar nickname";
+        } elseif (isset($_POST["delete_button"])) {
+            $txtContent = str_replace($search, '', $txtContent);
+            $success_message_txt = "Nickname excluído com sucesso!";
+            $error_message_txt = "Erro ao excluir nickname";
+        }
+
+        if (file_put_contents($txtFilename, $txtContent)) {
+            echo $success_message_txt;
+        } else {
+            echo $error_message_txt;
+        }
+    }
+
+    // Fechar a conexão com o banco de dados
+    $conn->close();
 }
 ?>
